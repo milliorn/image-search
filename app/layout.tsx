d0f194aa6f94bf79f7dcfc35cff0456177a24f21
milliorn <scottmilliorn@gmail.com>
2024-03-16 15:20:25 -0700

    Add viewport properties to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,10 @@ export const viewport: Viewport = {
     { media: "(prefers-color-scheme: light)", color: "cyan" },
     { media: "(prefers-color-scheme: dark)", color: "black" },
   ],
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export const metadata: Metadata = {
