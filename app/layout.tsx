7f2b4a6c7287ed0a78e9d04d6a8d6475c2b8ca46
milliorn <scottmilliorn@gmail.com>
2024-03-16 15:01:58 -0700

    Add metadata and format detection to layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,24 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { url } from "inspector";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Image Search",
   description: "Search for images using the Unsplash API",
+  generator: "Next.js",
+  applicationName: "Image Search",
+  keywords: ["images", "search", "unsplash"],
+  authors: [{ name: "Scott Milliorn", url: "https://milliorn.xyz/" }],
+  creator: "Scott Milliorn",
+  publisher: "Scott Milliorn",
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
 };
 
 export default function RootLayout({
