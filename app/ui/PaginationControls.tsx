141ce742aa683110bc1f82f2562d6c52210cabe1
milliorn <scottmilliorn@gmail.com>
2024-04-02 15:57:25 -0700

    Add pagination input and go to page functionality

--- app/ui/PaginationControls.tsx
+++ app/ui/PaginationControls.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { ChangeEvent, useState } from "react";
 import { PaginationControlsProps } from "../models/UIComponentProps";
 
 /**
@@ -17,26 +18,55 @@ const PaginationControls = ({
   setPage,
   totalPages,
 }: PaginationControlsProps): JSX.Element => {
+  const [inputPage, setInputPage] = useState(page);
+
+  const handlePageChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const pageNum = parseInt(e.target.value, 10);
+    if (pageNum >= 1 && pageNum <= totalPages) {
+      setInputPage(pageNum);
+    }
+  };
+
+  const goToPage = () => {
+    setPage(inputPage);
+  };
+
   return (
-    <div className="flex justify-between sm:justify-evenly mt-4">
-      <button
-        disabled={page === 1}
-        className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
-        onClick={() => setPage(page - 1)}
-      >
-        Previous
-      </button>
-      <span className="text-center mt-2">
-        {totalPages === 0 ? "" : `Page ${page} of ${totalPages}`}
-      </span>
-      <button
-        disabled={page === totalPages}
-        className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
-        onClick={() => setPage(page + 1)}
-      >
-        Next
-      </button>
-    </div>
+    <>
+      <div className="flex justify-between sm:justify-evenly mt-4">
+        <button
+          disabled={page === 1}
+          className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => setPage(page - 1)}
+        >
+          Previous
+        </button>
+
+        <span className="text-center mt-2 text-sm sm:text-base">
+          {totalPages === 0 ? "Page 0 of 0" : `Page ${page} of ${totalPages}`}
+        </span>
+
+        <button
+          disabled={page === totalPages}
+          className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => setPage(page + 1)}
+        >
+          Next
+        </button>
+      </div>
+      {/* <div className="mx-auto text-center my-4">
+        <button className="text-center mt-2 mr-2 border rounded px-2 py-2 bg-gray-500">
+          Go to Page
+        </button>
+        <input
+          type="text"
+          className="text-center w-16 text-black"
+          value={inputPage}
+          onChange={handlePageChange}
+          onBlur={goToPage}
+        />
+      </div> */}
+    </>
   );
 };
 
