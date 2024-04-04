cdab4ee4cb2b85ed6cc6922dcea39ae44a074a78
milliorn <scottmilliorn@gmail.com>
2024-04-04 00:00:16 -0700

    Add Footer component to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Footer from "./ui/Footer";
 
 /**
  * Initializes the Inter font with the specified subsets.
@@ -138,7 +139,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children} <Footer />
+      </body>
     </html>
   );
 }
