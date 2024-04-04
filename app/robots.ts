41b26a3b2cafbd13c4564dff902ead0785f9fabd
milliorn <scottmilliorn@gmail.com>
2024-04-04 15:11:13 -0700

    Refactor robots.ts and Footer.tsx files

--- app/robots.ts
+++ app/robots.ts
@@ -1,5 +1,10 @@
 import { MetadataRoute } from 'next'
 
+/*
+  * Represents the robots.txt configuration.
+  * https://nextjs.org/docs/app/api-reference/file-conventions/metadata/robots
+*/
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
