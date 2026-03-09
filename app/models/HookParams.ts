d2a92d18fa7d26e69da6505a74d0360e77ee1488
Scott Milliorn <scottmilliorn@gmail.com>
2026-03-09 13:23:00 -0700

    Refactor HookParams types to use RefObject for searchInput and remove unused lint scripts from package.json

--- app/models/HookParams.ts
+++ app/models/HookParams.ts
@@ -5,13 +5,13 @@ import type * as React from "react";
 type HandleInputChangeParams = {
   setPage: React.Dispatch<React.SetStateAction<number>>;
   fetchImages: (q?: string, page?: number) => void | Promise<void>;
-  searchInput: React.MutableRefObject<HTMLInputElement | null>;
+  searchInput: React.RefObject<HTMLInputElement>;
 };
 
 type UseSelectionHandler = {
   setPage: React.Dispatch<React.SetStateAction<number>>;
   fetchImages: (q: string, page: number) => void | Promise<void>;
-  searchInput: React.MutableRefObject<HTMLInputElement | null>;
+  searchInput: React.RefObject<HTMLInputElement>;
 };
 
 export type { HandleInputChangeParams, UseSelectionHandler };
