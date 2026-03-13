fa79c17aa397113819c16e4c16cea1052623d4af
Scott Milliorn <scottmilliorn@gmail.com>
2026-03-12 18:31:10 -0700

    Enhance pagination button logic to disable 'Go' button based on input validity

--- app/ui/PaginationControls.tsx
+++ app/ui/PaginationControls.tsx
@@ -112,7 +112,12 @@ const PaginationControls = ({
         </button>
         <button
           className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
-          disabled={loading}
+          disabled={
+            loading ||
+            isNaN(parsedInputPage) ||
+            parsedInputPage < 1 ||
+            parsedInputPage > totalPagesMax
+          }
           onClick={goToPage}
         >
           Go
