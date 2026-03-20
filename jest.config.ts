6db78d1cd5c0ede7215ce9f207a486eb383de909
Scott Milliorn <scottmilliorn@gmail.com>
2026-03-20 14:16:47 -0700

    Add Jest configuration for testing setup

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const config: Config = {
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  collectCoverageFrom: [
+    "app/**/*.{ts,tsx}",
+    "!app/models/**",
+    "!app/layout.tsx",
+    "!app/manifest.ts",
+    "!app/robots.ts",
+    "!app/sitemap.ts",
+  ],
+};
+
+export default createJestConfig(config);
