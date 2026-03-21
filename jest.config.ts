1ba22d353239e4b0aa0dfef1cca2fce73e3abd4a
Scott Milliorn <scottmilliorn@gmail.com>
2026-03-21 16:03:08 -0700

    Update Jest configuration to include moduleNameMapper and testPathIgnorePatterns

--- jest.config.ts
+++ jest.config.ts
@@ -10,6 +10,10 @@ const config: Config = {
   testEnvironment: "jsdom",
   roots: ["<rootDir>/__tests__"],
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleNameMapper: {
+    "^next/link$": "<rootDir>/__mocks__/next/link.tsx",
+  },
+  testPathIgnorePatterns: ["/node_modules/", "<rootDir>/__tests__/fixtures/"],
   collectCoverageFrom: [
     "app/**/*.{ts,tsx}",
     "!app/models/**",
